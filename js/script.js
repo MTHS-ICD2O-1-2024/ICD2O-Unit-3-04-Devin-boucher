63e01054ab2b7d28f0c1e7b5982a0801f691c3b0
default <devin.boucher@stu.ocsb.ca>
2025-04-08 19:53:51 +0000

    made code for website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Devin Boucher
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Converts Fahrenheit to Celsius
+ */
+function Calculate() {
+  // Get input values
+  const Fahrenheit = parseFloat(document.getElementById("Fahrenheit").value)
+
+  // Convert Fahrenheit to Celsius using the formula: (F - 32) * 5 / 9
+  const Celsius = (Fahrenheit - 32) * 5 / 9
+
+  // Display the result
+  document.getElementById("Answer").innerHTML = `The temperature in Celsius is: <strong>${Celsius.toFixed(3)}</strong>°C`
+}
